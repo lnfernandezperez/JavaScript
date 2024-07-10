a5eac22e880a79e2176ccdcf1e7c7b16385d7ea6
Lautaro Fernandez <lautaro.fernandez@gm2dev.com>
2024-07-10 12:56:04 -0300

    feat(java): first java exercise

---
+++ TPVariables.js
@@ -0,0 +1,14 @@
+let chanchitoFeliz = "Vive feliz en su granja";
+
+let myAge = 18;
+
+let imLearning = true;
+
+let myAddress = "Hilarion de la Quintana";
+let granyAddress = "Ayacucho";
+let familyAddress = myAddress + granyAddress;
+
+let age = 18;
+let height = 180;
+
+let sum = age + height;
