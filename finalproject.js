beb9a39c8239a46ec606d5d6cf4035a7bc863ab5
Lautaro Fernandez <lautaro.fernandez@gm2dev.com>
2024-08-01 11:42:55 -0300

    feat: add prompt to exercise

--- finalproject.js
+++ finalproject.js
@@ -20,6 +20,7 @@ function gestionarPedido(nombre, edad, cantidad, precioUnitario, metodoPago, hor
     console.log("Resumen del pedido:");
     console.log(`Cliente: ${nombre}`);
     console.log(`Edad: ${edad}`);
+    console.log(`Producto: ${producto}`);
     console.log(`Cantidad: ${cantidad}`);
     console.log(`Precio unitario: $${precioUnitario}`);
     console.log(`Costo total antes del descuento: $${cantidad * precioUnitario}`);
@@ -29,52 +30,60 @@ function gestionarPedido(nombre, edad, cantidad, precioUnitario, metodoPago, hor
     console.log(`Costo final del pedido: $${costoFinal.toFixed(2)}`);
 }
 
- function saludarCliente(nombre) {
+function saludarCliente(nombre) {
+
         console.log(`¡Hola, ${nombre}! Bienvenido(a) a nuestro restaurante.`);
     }
 
- function verificarEdad(edad) {
+    function verificarEdad(edad) {
         if (edad < 18) {
             console.log("Advertencia: No puedes consumir bebidas alcohólicas.");
         }
     }
-
-function calcularCostoTotal(cantidad, precioUnitario) {
+    function verificarStock(cantidad, producto){
+        switch(producto.toLowerCase()){
+            case "manzanas":
+                console.log('${cantidad} de manzanas')
+        }
+    
+}
+    function calcularCostoTotal(cantidad, precioUnitario) {
         return cantidad * precioUnitario;
     }
 
-function aplicarDescuento(costoTotal, edad) {
+    function aplicarDescuento(costoTotal, edad) {
     if (edad > 60) {
         return costoTotal * 0.90; 
         }
         return costoTotal;
     }
 
-function determinarMetodoPago(metodoPago, costoTotal) {
+    function determinarMetodoPago(metodoPago, costoTotal) {
     if (metodoPago === 'Efectivo') {
         return costoTotal * 0.90; 
         }
         return costoTotal;
     }
 
- function verificarHoraPedido(hora) {
+    function verificarHoraPedido(hora) {
       
         if (hora >= 12 && hora <= 14) {
             console.log("Es hora pico, puede haber demoras en la entrega del pedido.");
         }
     }
 
-function calcularImpuesto(costoTotal) {
+    function calcularImpuesto(costoTotal) {
     return costoTotal * 0.05; 
     }
 
 
-
-const nombre = 'Juan';
-const edad = 65;
-const cantidad = 3;
+let nombre = prompt("Ingrese su nombre");
+let edad = prompt("Ingrese su edad");
+let producto = prompt("Que va a comprar hoy?")
+let cantidad = prompt("Cuantas va a llevar?")
 const precioUnitario = 150;
-const metodoPago = 'Efectivo';
+let metodoPago = prompt("Como elige abonar la compra?")
 const horaPedido = 13;
 
 gestionarPedido(nombre, edad, cantidad, precioUnitario, metodoPago, horaPedido);
+
