af7be47e9eac3152ce174adf8645e048366a114d
Lautaro Fernandez <lautaro.fernandez@gm2dev.com>
2024-08-19 12:45:49 -0300

    new exercise with DOM 19/08

---
+++ DOM.js
@@ -0,0 +1,40 @@
+let intro = document.querySelector("intro");
+
+intro.document.add("del video");
+console.log(intro);
+
+let title = document.getElementById(header - title).innerHTML("New Title");
+
+let ulList = document.querySelectorAll(".lista");
+
+ulList.forEach(function (li) {
+  console.log(li);
+});
+
+let remove = document.querySelector(".warning");
+remove.classList.remove("warning");
+
+let principalTitle = document.querySelector("h1.main-title");
+principalTitle.textContent = "Bienvenidos";
+
+let footer = document.getElementById("footer");
+
+footer.style.backgroundColor = "lightblue";
+
+let divContent = document.querySelector("div.content");
+
+let nuevoParrafo = document.createElement("p");
+
+nuevoParrafo.createElement;
+
+let elementoHeader = document.querySelector(".header");
+
+if (elementoHeader) {
+  let tieneClaseSticky = elementoHeader.classList.contains("sticky");
+
+  if (tieneClaseSticky) {
+    console.log("El elemento tiene la clase 'sticky'.");
+  } else {
+    console.log("El elemento no tiene la clase 'sticky'.");
+  }
+}
