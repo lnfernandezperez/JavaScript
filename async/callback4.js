bb1136aaaeb8e821fcfc3a0a3c1490cfae15e7a6
Lautaro Fernandez <lautaro.fernandez@gm2dev.com>
2024-09-27 15:35:01 -0300

    feat exercise 4

--- async/callback4.js
+++ async/callback4.js
@@ -1,33 +1,42 @@
-function assemblePart1(callback){
-    crossOriginIsolated.log('Assembling part 1 started...')
-    setTimeout(() => {
-        console.log('Part 1 assmble finished')
-        callback()
-    }, 1000);
+function assemblePart1(callback) {
+  console.log("Assembling part 1 started....");
+  setTimeout(function () {
+    const isError = Math.random() < 0.3;
+    if (isError) {
+      console.error("There was an error assembling part 1 oh nooooo");
+      return;
+    }
+
+    console.log("Part 1 assmble finished");
+    callback();
+  }, 1000);
 }
-function assemblePart2(callback){
-    crossOriginIsolated.log('Assembling part 2 started...')
-    setTimeout(() => {
-        const isError = false;
-        if (isError){
-            console.error('There is an error')
-        } else{
-        console.log('Part 2 assmble finished')
-        callback()
-        }
-    }, 3000);
+
+function assemblePart2(callback) {
+  console.log("Assembling part 2 started....");
+  setTimeout(function () {
+    const isError = Math.random() < 0.5;
+    if (isError) {
+      console.error("There was an error assembling part 2 oh nooooo");
+    } else {
+      console.log("Part 2 assmble finished");
+      callback();
+    }
+  }, 4000);
 }
-function assemblePart3(callback){
-    crossOriginIsolated.log('Assembling part 3 started...')
-    setTimeout(() => {
-        console.log('Part 3 assmble finished')
-        callback()
-    }, 2500);
+
+function assemblePart3(callback) {
+  console.log("Assembling part 3 started....");
+  setTimeout(function () {
+    console.log("Part 3 assmble finished");
+    callback();
+  }, 2500);
 }
-function assemblePart4(callback){
-    crossOriginIsolated.log('Assembling part 4 started...')
-    setTimeout(() => {
-        console.log('Part 4 assmble finished')
-        callback()
-    }, 4000);
-}
+
+assemblePart1(function () {
+  assemblePart2(function () {
+    assemblePart3(function () {
+      console.log("Termine loco aguante");
+    });
+  });
+});
